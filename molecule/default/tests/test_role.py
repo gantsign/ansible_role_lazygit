ea97dcc18714fce21cf96084adf1a4a82818f65c
John Freeman <john.freeman.opensource@gmail.com>
2019-10-31 19:41:43 +0000

    Implemented lazygit role
    Role to configure lazygit.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -7,9 +7,5 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_lazygit(host):
+    assert host.run('lazygit -v').rc == 0

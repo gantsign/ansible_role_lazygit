4885328f7da81a2ca1937273c01a3e2f134b13fd
John Freeman <john.freeman.opensource@gmail.com>
2020-10-10 13:39:48 +0100

    Updated Molecule to 3.0.8
    Keeping up with the latest changes.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -1,11 +1,2 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
-
-
 def test_lazygit(host):
     assert host.run('lazygit -v').rc == 0
